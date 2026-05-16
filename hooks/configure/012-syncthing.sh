3ac94e1ff7152140fb0734392f1075497d39b59d
Fletcher Nichol <fnichol@nichol.ca>
2026-05-16 00:08:18 -0600

    feat(sync): start Homebrew Syncthing service
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- hooks/configure/012-syncthing.sh
+++ hooks/configure/012-syncthing.sh
@@ -19,5 +19,19 @@ configure_hook_syncthing() {
         indent as_root systemctl enable --now "$svc"
       fi
       ;;
+    macos)
+      local svc="syncthing"
+
+      local loaded
+      loaded="$(
+        brew services info "$svc" --json \
+          | jq -r --arg svc "$svc" '.[] | select(.name = $svc) | .loaded'
+      )"
+
+      if [ "$loaded" != "true" ]; then
+        info "Starting '$svc' service"
+        indent brew services start "$svc"
+      fi
+      ;;
   esac
 }
